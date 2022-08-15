186e7c5d42f00a2c0373f33a4958d3a70a2ad1b8
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2022-08-15 18:42:04 -0500

    week5 mergesort.py complete

--- week5/sort/mergesort.py
+++ week5/sort/mergesort.py
@@ -1,8 +1,15 @@
-
-
 def mergeSort(nlist):
     print("Splitting ",nlist)
+
     # insert your code to complete the mergeSort function
+    list_size = len(nlist)
+    mid_index = (list_size / 2)
+    if len(nlist) > 1:
+        left = nlist[:int(mid_index)]
+        right = nlist[int(mid_index):]
+        mergeSort(left)
+        mergeSort(right)
+        merge(nlist, left, right)
     print("Merging ",nlist)
 
 def merge(nlist,lefthalf,righthalf):
@@ -15,15 +22,28 @@ def merge(nlist,lefthalf,righthalf):
             nlist[k]=righthalf[j]
             j=j+1
         k=k+1
-
     while i < len(lefthalf):
         nlist[k]=lefthalf[i]
         i=i+1
         k=k+1
-
     while j < len(righthalf):
         nlist[k]=righthalf[j]
         j=j+1
         k=k+1
     return nlist
+
+# Allow user to input list and target
+usr_lst = input('Provide an unordered list of numbers to sort: ')
+stringified_lst = usr_lst.strip('][').split(', ')
+parsed_lst = []
+
+# Parse list from user for search function
+def parse(list):
+    if len(list) > 0:
+        parsed_lst.append(int(list[0]))
+        list.pop(0)
+        return parse(list)
+    return
+parse(stringified_lst)
+mergeSort(parsed_lst)
 
