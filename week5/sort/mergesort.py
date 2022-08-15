53b04e39eda86c9a78181bd2a1f99767197dc29c
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2022-08-14 23:30:02 -0500

    merge_sort

---
+++ week5/sort/mergesort.py
@@ -0,0 +1,29 @@
+
+
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    # insert your code to complete the mergeSort function
+    print("Merging ",nlist)
+
+def merge(nlist,lefthalf,righthalf):
+    i=j=k=0       
+    while i < len(lefthalf) and j < len(righthalf):
+        if lefthalf[i] < righthalf[j]:
+            nlist[k]=lefthalf[i]
+            i=i+1
+        else:
+            nlist[k]=righthalf[j]
+            j=j+1
+        k=k+1
+
+    while i < len(lefthalf):
+        nlist[k]=lefthalf[i]
+        i=i+1
+        k=k+1
+
+    while j < len(righthalf):
+        nlist[k]=righthalf[j]
+        j=j+1
+        k=k+1
+    return nlist
+
