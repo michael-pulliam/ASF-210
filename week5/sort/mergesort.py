631e3d46ce276e426dce508f5efb7ac6abe8d8f0
michael-pulliam <michael.pulliam@bryanuniversity.edu>
2022-08-15 19:14:31 -0500

    week5 mergesort resubmit for mastery

--- week5/sort/mergesort.py
+++ week5/sort/mergesort.py
@@ -1,52 +1,40 @@
-def mergeSort(nlist):
-    print("Splitting ",nlist)
+
 
     # insert your code to complete the mergeSort function
-    list_size = len(nlist)
-    mid_index = (list_size / 2)
-    if len(nlist) > 1:
-        left = nlist[:int(mid_index)]
-        right = nlist[int(mid_index):]
-        mergeSort(left)
-        mergeSort(right)
-        merge(nlist, left, right)
-    print("Merging ",nlist)
+def mergeSort(nlist):
+    print("Splitting ",nlist)
+    if len(nlist)>1:
+        mid = len(nlist)//2
+        lefthalf = nlist[:mid]
+        righthalf = nlist[mid:]
 
-def merge(nlist,lefthalf,righthalf):
-    i=j=k=0       
-    while i < len(lefthalf) and j < len(righthalf):
-        if lefthalf[i] < righthalf[j]:
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+        i=j=k=0       
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                nlist[k]=lefthalf[i]
+                i=i+1
+            else:
+                nlist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
             nlist[k]=lefthalf[i]
             i=i+1
-        else:
+            k=k+1
+
+        while j < len(righthalf):
             nlist[k]=righthalf[j]
             j=j+1
-        k=k+1
-    while i < len(lefthalf):
-        nlist[k]=lefthalf[i]
-        i=i+1
-        k=k+1
-    while j < len(righthalf):
-        nlist[k]=righthalf[j]
-        j=j+1
-        k=k+1
-    return nlist
+            k=k+1
+    print("Merging ",nlist)
 
 
-list = [ 55 ,  31 ,  26 ,  20 ,  63 ,  7 ,  51 ,  74 ,  81 ,  40 ]
-mergeSort(list)
-# # Allow user to input list and target
-# usr_lst = input('Provide an unordered list of numbers to sort: ')
-# stringified_lst = usr_lst.strip('][').split(', ')
-# parsed_lst = []
 
-# # Parse list from user for search function
-# def parse(list):
-#     if len(list) > 0:
-#         parsed_lst.append(int(list[0]))
-#         list.pop(0)
-#         return parse(list)
-#     return
-# parse(stringified_lst)
-# mergeSort(parsed_lst)
+
+nlist = [ 55 ,  31 ,  26 ,  20 ,  63 ,  7 ,  51 ,  74 ,  81 ,  40 ]
+mergeSort(nlist)
+print(nlist)
 
